f96116248161312145ebfcef0899938016db919c
Saksham Arora <saksham10arora@gmail.com>
2025-01-21 16:13:00 +0530

    Implement C++ systems module: ThreadManagement.cpp

---
+++ cpp_mastery/03_Concurrency/ThreadManagement.cpp
@@ -0,0 +1,169 @@
+/**
+ * Topic: Concurrency (Threads and Mutexes)
+ */
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+std::mutex mtx;
+int shared_counter = 0;
+
+void increment(int n) {
+    for (int i = 0; i < n; ++i) {
+        std::lock_guard<std::mutex> lock(mtx);
+        shared_counter++;
+    }
+}
+
+int main() {
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back(increment, 10000);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    std::cout << "Final counter: " << shared_counter << "\n";
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
