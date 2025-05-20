4d558487f30ca88c28a3a78f8244668d2bc55e39
Saksham Arora <saksham10arora@gmail.com>
2025-05-20 18:06:00 +0530

    Implement C++ systems module: MoveSemantics.cpp

---
+++ cpp_mastery/01_Core_Language/MoveSemantics.cpp
@@ -0,0 +1,189 @@
+/**
+ * Topic: Move Semantics & Rvalue References
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+
+class MemoryBuffer {
+    double* data_;
+    size_t size_;
+public:
+    MemoryBuffer(size_t n) : size_(n), data_(new double[n]) {
+        std::cout << "Allocated " << n << " doubles\n";
+    }
+    ~MemoryBuffer() { delete[] data_; }
+
+    // Move Constructor
+    MemoryBuffer(MemoryBuffer&& other) noexcept 
+        : data_(other.data_), size_(other.size_) {
+        other.data_ = nullptr;
+        other.size_ = 0;
+        std::cout << "Moved buffer\n";
+    }
+
+    // Move Assignment
+    MemoryBuffer& operator=(MemoryBuffer&& other) noexcept {
+        if (this != &other) {
+            delete[] data_;
+            data_ = other.data_;
+            size_ = other.size_;
+            other.data_ = nullptr;
+            other.size_ = 0;
+        }
+        return *this;
+    }
+
+    // Disable Copy for demonstration
+    MemoryBuffer(const MemoryBuffer&) = delete;
+    MemoryBuffer& operator=(const MemoryBuffer&) = delete;
+};
+
+MemoryBuffer create_large_buffer() {
+    return MemoryBuffer(1000000); // RVO or Move
+}
+
+int main() {
+    MemoryBuffer b1 = create_large_buffer();
+    MemoryBuffer b2 = std::move(b1); // Explicit move
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
